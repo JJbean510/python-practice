b043d5653806e57682ac9ed6f4b805fe1307a788
JJbean510 <jerellparker1234@gmail.com>
2020-07-06 12:38:22 -0700

    1st commit

---
+++ Practice/my_module.py
@@ -0,0 +1,19 @@
+# Module Practice
+
+import datetime
+
+name = ''
+name = input('Enter your name: ')
+
+def greeting(name):
+   print('Hello, ' + name)
+
+# x = datetime.datetime.now()
+# def date_month():
+#    print(x.strftime('%B' ) + ', It\'s gonna be a how one!')
+
+person1 = {
+   'name': 'Jerell',
+   'age': 26,
+   'country': 'Norway'
+   }
